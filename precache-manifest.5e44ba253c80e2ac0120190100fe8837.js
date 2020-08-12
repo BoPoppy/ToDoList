2a6c6789fbb523948d5c0f9fbe0cc9bd985f0817
Thong Vo <hoangthongvo_1993@yahoo.com.vn>
2020-08-12 15:34:44 +0700

    Updates

---
+++ precache-manifest.5e44ba253c80e2ac0120190100fe8837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "763b54301accf8cdf0e66925b2dc600d",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "bccb0e32e8a0e3242ac7",
+    "url": "/ToDoList/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "a37819642ba6679319ea",
+    "url": "/ToDoList/static/css/main.23374058.chunk.css"
+  },
+  {
+    "revision": "bccb0e32e8a0e3242ac7",
+    "url": "/ToDoList/static/js/2.f38fac6a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/ToDoList/static/js/2.f38fac6a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a37819642ba6679319ea",
+    "url": "/ToDoList/static/js/main.1289bc82.chunk.js"
+  },
+  {
+    "revision": "1fa5a9d07dd9ed65eede",
+    "url": "/ToDoList/static/js/runtime-main.6b7d4eda.js"
+  }
+]);
